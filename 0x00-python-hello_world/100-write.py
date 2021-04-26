d67099e60ad73c8a95842e7645a4659f2bf98b5c
dianaparr <dianaparra.9611@gmail.com>
2021-04-26 18:20:48 -0500

    Adding new line

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 import sys
-sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19") #Redirecting standard error information
-sys.exit(1) #Exiting current flow of execution
+# Redirecting standard error information
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+# Exiting current flow of execution
+sys.exit(1)
