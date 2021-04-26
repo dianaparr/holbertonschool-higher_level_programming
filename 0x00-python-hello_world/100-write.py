136ae27fff4c3a878a24c2d1057774bf4dcd1d31
dianaparr <dianaparra.9611@gmail.com>
2021-04-26 18:06:43 -0500

    Use of sys module

---
+++ 0x00-python-hello_world/100-write.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+import sys
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19") #Redirecting standard error information
+sys.exit(1) #Exiting current flow of execution
