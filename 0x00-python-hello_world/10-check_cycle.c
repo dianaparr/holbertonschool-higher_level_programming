4ba6c3ff73eccf0c25cafd97c84b1c76d5a45fea
dianaparr <dianaparra.9611@gmail.com>
2021-04-26 22:24:28 -0500

    Check if a linked list has a cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,26 +1,28 @@
 #include "lists.h"
 
 /**
- *check_cycle - 
- *@list: 
- *Return: 
+ *check_cycle - Function checks if a singly linked list has a cycle in it
+ *@list: head of the linked list
+ *Return: 0, if there is no cycle and 1, if there is a cycle
+ *
+ * Description: Use of Floyd's algorithm
  */
 int check_cycle(listint_t *list)
 {
-    /* Declaro dos punteros y los inicializo al inicio de la lista */
-    listint_t *ptr_slow = NULL, *ptr_fast = NULL;
+    /* Declare two pointers and initialize them at the beginning of the list */
+    listint_t *ptr_one = NULL, *ptr_two = NULL;
 
-    ptr_slow = list;
-    ptr_fast = list;
+    ptr_one = list;
+    ptr_two = list;
 
-    while(ptr_slow && ptr_fast && ptr_fast->next)
+    while(ptr_one && ptr_two && ptr_two->next)
     {
-        /* ptr_slow avanza un nodo a la vez */
-        ptr_slow = ptr_slow->next;
-        /* ptr_fast avanza dos nodos a la vez */
-        ptr_fast = ptr_fast->next->next;
-        if (ptr_slow == ptr_fast)
+        /* ptr_one advances one node at the time */
+        ptr_one = ptr_one->next;
+        /* ptr_two advances two nodes at the time */
+        ptr_two = ptr_two->next->next;
+        /* if both pointers point to the same node, it is because there is a cycle */
+        if (ptr_one == ptr_two)
             return (1);
     }
     return (0);
-}
