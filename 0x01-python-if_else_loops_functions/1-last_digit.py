131f9647a3d1b3e3129ca2526367d37aad289890
dianaparr <dianaparra.9611@gmail.com>
2021-04-27 16:11:12 -0500

    Condition fix when zero

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,11 +6,11 @@ if number >= 0:
     last_dgt = number % 10
     if last_dgt > 5:
         print(template_string.format(number, last_dgt, "greater than 5"))
-    elif last_dgt < 6 != 0:
-        print(template_string.format(number, last_dgt, "\
-less than 6 and not 0"))
-    else:
+    elif last_dgt == 0:
         print(template_string.format(number, last_dgt, "0"))
+    else:
+        print(template_string.format(number, last_dgt,
+              "less than 6 and not 0"))
 else:
     # convert a negative number to positive
     convert_negative = number * (-1)
@@ -19,5 +19,5 @@ else:
         print(template_string.format(number, last_dgt, "0"))
     else:
         # the last digit is printed with "-" sign
-        print(template_string.format(number, last_dgt * (-1), "\
-less than 6 and not 0"))
+        print(template_string.format(number, last_dgt * (-1),
+              "less than 6 and not 0"))
