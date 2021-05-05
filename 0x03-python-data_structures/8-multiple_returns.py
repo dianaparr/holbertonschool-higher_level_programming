5081dbb7efb81df8b68153b02fe25b0b66d6bbcd
dianaparr <dianaparra.9611@gmail.com>
2021-05-05 18:16:26 -0500

    Check the error

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,7 +3,7 @@ def multiple_returns(sentence):
     if len(sentence) is None:
         tuple_two = len(sentence), None
         return tuple_two
-    else:
+    if len(sentence) > 0:
         # other form represents position 0 of the string with splice
         tuple_two = len(sentence), sentence[:1]
         return tuple_two
