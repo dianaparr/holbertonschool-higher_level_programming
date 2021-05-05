c17c2b04d0f1fd26741bdfa85642a798dbfbf2f5
dianaparr <dianaparra.9611@gmail.com>
2021-05-05 16:56:44 -0500

    add end in first print

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     if my_list == 0:
-        print("{}".format(""))
+        print("{}".format(""), end="")
     # reverse() method: directly modifies the original list
     my_list.reverse()
     for element in my_list:
