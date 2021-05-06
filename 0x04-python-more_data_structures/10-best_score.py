0ca4da1a1beb3b18f1b34d72ec6cb4d576e7112a
dianaparr <dianaparra.9611@gmail.com>
2021-05-06 17:07:46 -0500

    Add comment

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -5,6 +5,8 @@ def best_score(a_dictionary):
         key = ""
         best_s = 0
         for v in a_dictionary:
+            # If the value is bigger that the score, the variables key
+            # and best_s take their corresponding values
             if a_dictionary.get(v) > best_s:
                 key = v
                 best_s = a_dictionary.get(v)
