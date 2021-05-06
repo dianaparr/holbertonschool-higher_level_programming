a94669a981321c755bb352ce6d6077cf85aada45
dianaparr <dianaparra.9611@gmail.com>
2021-05-05 20:23:05 -0500

    Change len sentence with string empty

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if len(sentence) is None:
+    if len(sentence) == "":
         tuple_two = len(sentence), None
         return tuple_two
-    if len(sentence) > 0:
+    else:
         # other form represents position 0 of the string with splice
         tuple_two = len(sentence), sentence[:1]
         return tuple_two
