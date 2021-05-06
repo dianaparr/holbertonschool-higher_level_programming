24b236e7b9371466ffc48b233c016897cbf4257d
dianaparr <dianaparra.9611@gmail.com>
2021-05-06 12:44:22 -0500

    Fixing a comment

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
-    # Use len() and key() method because a integer is expect
+    # Use len() and keys() method because a integer is expect
     return len(list(a_dictionary.keys()))
