c006b8c558653673a9a03874cc8ed8143577b13a
dianaparr <dianaparra.9611@gmail.com>
2021-05-05 21:10:48 -0500

    Add first condition if the list exist

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    if my_list == 0:
-        print("{}".format(""), end="")
-    # reverse() method: directly modifies the original list
-    my_list.reverse()
-    for element in my_list:
-        print("{:d}".format(element))
+    if my_list is not None:
+        # reverse() method: directly modifies the original list
+        my_list.reverse()
+        for element in my_list:
+            print("{:d}".format(element))
