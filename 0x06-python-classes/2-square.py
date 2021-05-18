a7197e7b06565a01c02e48b023a8f0014cf29c75
dianaparr <dianaparra.9611@gmail.com>
2021-05-18 17:35:22 -0500

    add docstrings

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+""" Define a class called Square """
+
+
 class Square:
+    """ Constructor method to initialize the attributes of the
+    instantiated object 'size' """
     def __init__(self, size=0):
+        """ validate the 'size' attribute with several conditions """
         # other way: type(size) != int
         if type(size) is not int:
             raise TypeError("size must be an integer")
