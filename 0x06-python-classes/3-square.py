2c69be41f5cfb5644f1c7b0d089988b7947ca463
dianaparr <dianaparra.9611@gmail.com>
2021-05-18 17:43:17 -0500

    add docstrings

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+""" Define a class called Square """
+
+
 class Square:
+    """ Constructor method to initialize the attributes of the
+    instantiated object 'size' """
     def __init__(self, size=0):
+        """ validate the 'size' attribute with several conditions """
         if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
@@ -10,4 +16,6 @@ class Square:
 
     # class method. Access to attribute with self.__size
     def area(self):
+        """ This class method returns the square of what is
+            in the attribute 'size' """
         return self.__size ** 2
