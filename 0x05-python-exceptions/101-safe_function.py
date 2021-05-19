d100fcc70f4f36b32b1bddff323ae190acc30fe1
dianaparr <dianaparra.9611@gmail.com>
2021-05-19 11:28:55 -0500

    function that executes a function

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,11 @@
+#!/bin/usr/python3
+import sys
+
+
+def safe_function(fct, *args):
+    try:
+        result = fct(*args)
+        return result
+    except Exception as err_msg:
+        print("Exception: {}".format(err_msg), file=sys.stderr)
+        return None
