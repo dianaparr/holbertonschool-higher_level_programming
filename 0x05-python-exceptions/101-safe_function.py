5e40727ec2f0fed65da991c606070fa4f3ba88ea
dianaparr <dianaparra.9611@gmail.com>
2021-05-19 11:32:49 -0500

    change name err

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -6,6 +6,6 @@ def safe_function(fct, *args):
     try:
         result = fct(*args)
         return result
-    except Exception as err_msg:
-        print("Exception: {}".format(err_msg), file=sys.stderr)
+    except Exception as err:
+        print("Exception: {}".format(err), file=sys.stderr)
         return None
