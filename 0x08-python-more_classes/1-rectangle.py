9835cea9a33fd7e5074c1bf98e1835a376e473fc
dianaparr <dianaparra.9611@gmail.com>
2021-05-20 11:42:01 -0500

    create a init instantiation with two optionals parameters

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Create a class called Rectangle """
+
+
+class Rectangle:
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
