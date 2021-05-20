bf40abe8501cf56218e085c6e10200b8be71cb8f
dianaparr <dianaparra.9611@gmail.com>
2021-05-20 12:11:56 -0500

    add all the same from the previous item, 1-rectangle, verifying information required in the item

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+""" Create a class called Rectangle """
+
+
+class Rectangle:
+    """ Constructor method to initialize the attribute of the
+        instantiated object with two optionals parameters:
+
+        Args:
+            param1 (width): Is a private attribute, type int
+            param2 (height): Is a private attribute, type int
+        """
+    def __init__(self, width=0, height=0):
+        self.height = height
+        self.width = width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise TypeError("height must be >= 0")
+        else:
+            self.__height = value
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise TypeError("width must be >= 0")
+        else:
+            self.__width = value
