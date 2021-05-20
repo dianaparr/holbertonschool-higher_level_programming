4fb112ac9daa8f51ccbf5890ee4e76ab9ab099ae
dianaparr <dianaparra.9611@gmail.com>
2021-05-20 11:53:47 -0500

    create the private instance attribute width with its property decorator and the setter

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -2,8 +2,24 @@
 """ Create a class called Rectangle """
 
 
+from typing import Type
+
+
 class Rectangle:
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+    
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise TypeError("width must be >= 0")
+        else:
+            self.__value = value
 
