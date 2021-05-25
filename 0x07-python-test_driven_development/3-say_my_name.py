44bd2eee05ebe0bcc7523ef7a132edfb59f4f3ea
dianaparr <dianaparra.9611@gmail.com>
2021-05-24 22:04:22 -0500

    pep8 style revision and added docstring

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
-""" """
+"""
+Function that prints a string
+"""
+
+
 def say_my_name(first_name, last_name=""):
-    """ """
+    """ Function that prints a strings preceded by the
+        phrase: 'My name is <first_name> <last_name>'
+
+        Parameters:
+            first_name (string): first part of the sentence
+            last_name="" (string): second part of the sentence
+    """
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
