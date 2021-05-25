83de024d4c609f825ab582b2ec265d69d8a3c969
dianaparr <dianaparra.9611@gmail.com>
2021-05-25 13:04:39 -0500

    unittest with cases and docstrings

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,12 +1,38 @@
+#!/usr/bin/python3
+"""
+Import unittest and created a class for unit test
+"""
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
+    """ Class called TestMaxInteger. This would be a subclass of
+        the TestCase class of the unittest module. 
+    """
     def test_max_integer(self):
-        # self.assertRaises(max_integer((1, 2, 4), 4))
+        """ Method representing tests. In this case, which return
+            the max integer in a list of integers.
+        
+        Methods of the class TestCase:
+            assertEqual: make sure they are equal
+            assertNotEqual: make sure they are not the same
+            assertRaises: verifies the production of exceptions
+        """
         self.assertEqual(max_integer([1, 2, 3]), 3)
-        self.assertEqual(max_integer([-87, -900, -357]), -87)
+        self.assertNotEqual(max_integer([-87, -900, -357]), -900)
         self.assertEqual(max_integer([87.456, 87.455, 86]), 87.456)
+        self.assertEqual(max_integer([87.456, 87.456, 87.456]), 87.456)
+        self.assertEqual(max_integer([]), None)
+        self.assertEqual(max_integer([8]), 8)
+        self.assertEqual(max_integer((6, 7, 8)), 8)
+        with self.assertRaises(TypeError):
+            max_integer([86, 87.455, "h"])
+        with self.assertRaises(TypeError):
+            max_integer([6, [-7, 9, 10.89], 8, (60, 90)])
 
+""" Final block to run the tests.
+        unittest.main(): provides the command line interface
+                         for the test script.
+"""
 if __name__ == '__main__':
     unittest.main()
