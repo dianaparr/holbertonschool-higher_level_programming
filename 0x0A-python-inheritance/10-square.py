1d07a0460b9b0d79caa6f80fae9f1b4c8dae6283
dianaparr <dianaparra.9611@gmail.com>
2021-06-01 18:41:23 -0500

    change to .format print

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -29,5 +29,5 @@ class Square(Rectangle):
     """ Use of the special method __str__ that returns a string
         with what you want to show, for example, to an end user """
     def __str__(self):
-        show_mssg = f"[Rectangle] {self.__size}/{self.__size}"
+        show_mssg = "[Rectangle] {}/{}".format(self.__size, self.__size)
         return show_mssg
