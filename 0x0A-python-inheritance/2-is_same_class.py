2ec9d3956596e54bc7a13f4ade009ef0162250f7
dianaparr <dianaparra.9611@gmail.com>
2021-06-01 13:07:46 -0500

    verificated use type(), return type exactly, pep8 check and docstrings

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python3
-""" 
+"""
 Function that indicate object is
 exactly an instance of the specified class
 """
 
 
 def is_same_class(obj, a_class):
+    """
+    Parameters:
+        obj: object to evaluate
+        a_class: possible class to witch obj
+
+    Return:
+        True: if the object is exactly an instance
+              of the specified class,
+              otherwise False.
+    """
     if type(obj) is a_class:
         return True
     else:
