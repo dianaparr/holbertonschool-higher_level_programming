0521ac9b4e2b6fcf8b683979d2e3ddfd9f9940b8
dianaparr <dianaparra.9611@gmail.com>
2021-06-01 16:46:42 -0500

    implementation task completed, output ok, f-string print format

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Created a Class Rectangle and import module of class BaseGeometry
+"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ Constructor method to initialize the attribute of the
+        instantiated object with two optionals parameters
+        to be validated by integer_validator:
+
+        Args:
+            param1 (width): Is a private attribute
+            param2 (height): Is a private attribute
+    """
+    def __init__(self, width, height):
+        self.__width = width
+        # super().__init__(name, value)
+        super().integer_validator("width", width)
+        self.__height = height
+        super().integer_validator("height", height)
+
+    """ Public instance method that return
+        the area of a rectangle
+    """
+    def area(self):
+        return self.__width * self.__height
+
+    """ Use of the special method __str__ that returns a string
+        with what you want to show, for example, to an end user """
+    def __str__(self):
+        show_mssg = f"[Rectangle] {self.__width}/{self.__height}"
+        return show_mssg
