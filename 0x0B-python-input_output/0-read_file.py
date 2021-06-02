1ca7296332c69243fab035080b315163bd754c39
dianaparr <dianaparra.9611@gmail.com>
2021-06-02 12:01:24 -0500

    fixing print and pep8 style

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,12 +2,14 @@
 """
 Function that reads a text file
 """
+
+
 def read_file(filename=""):
-    """ Read file encoding utf-8 
+    """ Read file encoding utf-8
 
     Parameters:
             filename: the file to open and read
     """
     with open(filename, mode='r', encoding='UTF8') as f:
         file_to_read = f.read()
-    print("{}".format(file_to_read))
+    print("{}".format(file_to_read), end="")
