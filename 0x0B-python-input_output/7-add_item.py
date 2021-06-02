baf81b1c7bcfcae0824bdc03ed7c75a0b4825709
dianaparr <dianaparra.9611@gmail.com>
2021-06-02 17:06:40 -0500

    add args

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,10 +4,14 @@
 """
 
 from sys import argv
-import json
+import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 f_name = "add_item.json"
 # first step: open file for read
 list_argv = load_from_json_file(f_name)
+# adds args and save file
+for a in sys.argv[1:]:
+    list_argv.append(a)
+save_to_json_file(list_argv, f_name)
