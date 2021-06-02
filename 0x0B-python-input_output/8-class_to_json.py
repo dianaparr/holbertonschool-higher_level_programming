dc6b708429b1f67cd9ec094bb9b3599ed9df5a5a
dianaparr <dianaparra.9611@gmail.com>
2021-06-02 18:45:37 -0500

    return description dict

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Function that returns the dictionary description
+"""
+def class_to_json(obj):
+    """ Use attribute magic __dict__
+
+    Parameters:
+            obj: the object
+    """
+    return obj.__dict__
