efd2986b7cda116ddc37549c2870ef2eaaab41c6
dianaparr <dianaparra.9611@gmail.com>
2021-06-02 17:24:00 -0500

    add statement if name main

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,10 +8,11 @@ import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-f_name = "add_item.json"
-# first step: open file for read
-list_argv = load_from_json_file(f_name)
-# adds args and save file
-for a in sys.argv[1:]:
-    list_argv.append(a)
-save_to_json_file(list_argv, f_name)
+if __name__ == "__main__":
+    f_name = "add_item.json"
+    # first step: open file for read
+    list_argv = load_from_json_file(f_name)
+    # adds args and save file
+    for a in sys.argv[1:]:
+        list_argv.append(a)
+    save_to_json_file(list_argv, f_name)
