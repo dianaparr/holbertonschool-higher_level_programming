7cbb9d077e0b80eaac3c79c8e6e6ca1ce12094af
dianaparr <dianaparra.9611@gmail.com>
2021-06-02 12:39:58 -0500

    write in the text file, output ok

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Function that writes a string to a text file
+"""
+def write_file(filename="", text=""):
+    """ Write in the file
+        Create the file if doesn't exist and
+        Overwrite the content of the file if it
+        already exists.
+
+        Paramenters:
+                filename(string): the file to write
+                text(string): number of bytes to write
+    """
+    with open(filename, mode='w+', encoding='UTF8') as f:
+        file_to_write = f.write(text)
+    return file_to_write
