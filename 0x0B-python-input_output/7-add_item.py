bf1662f8a0cf29cf48511dbd4284429c5441a5c1
dianaparr <dianaparra.9611@gmail.com>
2021-06-02 21:58:29 -0500

    eliminate the double import of sys

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,7 +3,6 @@
     a Python list 
 """
 
-from sys import argv
 import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
