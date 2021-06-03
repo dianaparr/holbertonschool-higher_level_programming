9a4aa966ea9e3cc43be0ed651b74a6417fa35677
dianaparr <dianaparra.9611@gmail.com>
2021-06-02 22:48:59 -0500

    retire if name main

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,13 +7,12 @@ import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-if __name__ == "__main__":
-    # first step: open file for read
-    try:
-        list_argv = load_from_json_file("add_item.json")
-    except:
-        list_argv = list()
-    # adds args and save file
-    for a in sys.argv[1:]:
-        list_argv.append(a)
-    save_to_json_file(list_argv, "add_item.json")
+# first step: open file for read
+try:
+    list_argv = load_from_json_file("add_item.json")
+except:
+    list_argv = list()
+# adds args and save file
+for a in sys.argv[1:]:
+    list_argv.append(a)
+save_to_json_file(list_argv, "add_item.json")
