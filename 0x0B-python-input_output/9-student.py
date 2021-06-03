53167d0587a7d2b5d80cb16f4e91bdce804634ff
dianaparr <dianaparra.9611@gmail.com>
2021-06-02 20:27:21 -0500

    instantiation with public attributes and public method create

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Create a class called Student
+"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        """ Constructor method to initialize the attribute of the
+        instantiated object with two optionals parameters:
+        
+        Args:
+            param1 (first_name): Is a public attribute, type str
+            param2 (last_name): Is a public attribute, type str
+            param2 (age): Is a public attribute, type str
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    """ """
+    def to_json(self):
+        self.__dict__
