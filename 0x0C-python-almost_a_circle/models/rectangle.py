f61a8a35b849b8d0c57d6acbf600064686a802dd
dianaparr <dianaparra.9611@gmail.com>
2021-06-04 15:38:51 -0500

    create a class Rectangle and established the private attributes with getter/setter

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+"""
+Created a class called Rectangle inherits from Base
+"""
+
+from models.base import Base
+
+class Rectangle(Base):
+    """ Constructor method to initialize the attribute of the
+        instantiated object with four optionals parameters
+        and call to the super class Base with id:
+        
+        Args:
+            width: Is a private attribute
+            height: Is a private attribute
+            x: Is a private attribute
+            y: Is a private attribute
+    """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        # call the super class with id
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    """
+    Private instance attributes, each with its own public
+    getter and setter
+    """
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        pass
+    
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def width(self, height):
+        pass
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, x):
+        pass
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, y):
+        pass
