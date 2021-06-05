946767d2520f63b0510101ecb569a0b28765a151
dianaparr <dianaparra.9611@gmail.com>
2021-06-04 21:55:55 -0500

    new class that inheritance from rectangle

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Created a class called Square inherits from Rectangle
+"""
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """ Constructor method to initialize the attribute of the
+        instantiated object with four optionals parameters
+        and call to the super class Rectangle with id, x, y, width and height:
+
+        Args:
+            size: assigned to width and height
+            Use all attributes of Rectangle
+    """
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """ Overrinding the special method __str__ """
+        return "[{}] ({}) {}/{} - {}".format(__class__.__name__,
+                                                self.id, self.x, self.y,
+                                                self.width)
