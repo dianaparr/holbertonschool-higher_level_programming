f3644382ecab3476f699a6772ad0edd75755b69a
dianaparr <dianaparra.9611@gmail.com>
2021-06-04 22:29:46 -0500

    getter/setter with size because is the equal value (in square)

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -23,3 +23,17 @@ class Square(Rectangle):
         return "[{}] ({}) {}/{} - {}".format(__class__.__name__,
                                                 self.id, self.x, self.y,
                                                 self.width)
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, size):
+        if type(size) is not int:
+            raise TypeError("width must be an integer")
+        if size <= 0:
+            raise ValueError("width must be > 0")
+        else:
+            self.width = size
+            self.height = size
