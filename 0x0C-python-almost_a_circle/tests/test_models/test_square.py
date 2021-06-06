8cb18714a3baa16f69c51f31f6a0c6115a85ae65
dianaparr <dianaparra.9611@gmail.com>
2021-06-05 20:53:51 -0500

    file test with test basics documentation, pep8 style and shebang

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -3,45 +3,43 @@
 """
 import os
 import unittest
-from models.base import Base
-import models.base
+from models.square import Square
+import models.square
 import json
 
 
-class TestBase(unittest.TestCase):
-    """ Create a tests for the base class """
+class TestSquare(unittest.TestCase):
+    """ Create a tests for the square class """
     def test_style_pep8_model(self):
         """ PEP8 python style """
-        a = os.system("pep8 models/base.py")
+        a = os.system("pep8 models/square.py")
         self.assertEqual(a, 0)
 
     def test_style_pep8(self):
         """ PEP8 python style """
-        a = os.system("pep8 tests/test_models/test_base.py")
+        a = os.system("pep8 tests/test_models/test_square.py")
         self.assertEqual(a, 0)
 
     def test_shebang(self):
         """ Test shebang in the front line """
-        with open("models/base.py", mode='r') as f:
+        with open("models/square.py", mode='r') as f:
             r = f.read()
             b = r.splitlines()
             self.assertEqual(b[0], '#!/usr/bin/python3')
 
     def test_module_doc(self):
         """ Module with sufficient documentation """
-        self.assertTrue(len(models.base.__doc__) != 0)
+        self.assertTrue(len(models.square.__doc__) != 0)
 
     def test_class_doc(self):
         """ Class with sufficient documentation """
-        self.assertTrue(len(Base.__doc__) != 0)
+        self.assertTrue(len(Square.__doc__) != 0)
 
     def test_functions_doc(self):
         """ Functions with sufficient documentation """
-        self.assertTrue(len(Base.to_json_string.__doc__) != 0)
-        self.assertTrue(len(Base.save_to_file.__doc__) != 0)
-        self.assertTrue(len(Base.from_json_string.__doc__) != 0)
-        self.assertTrue(len(Base.create.__doc__) != 0)
-        self.assertTrue(len(Base.load_from_file.__doc__) != 0)
+        self.assertTrue(len(Square.__str__.__doc__) != 0)
+        self.assertTrue(len(Square.update.__doc__) != 0)
+        self.assertTrue(len(Square.to_dictionary.__doc__) != 0)
 
 
 if __name__ == "__main__":
