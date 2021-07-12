5828697e359801422a2a6a96cae8819ecf7e170e
dianaparr <dianaparra.9611@gmail.com>
2021-07-12 16:41:52 -0500

    Converts database and table to utf8 which collate

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,8 @@
+-- Convert to UTF-8 - MySQL server
+-- Convert database, table and filed name in first_table
+ALTER DATABASE hbtn_0c_0
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
+ALTER TABLE hbtn_0c_0.first_table
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
