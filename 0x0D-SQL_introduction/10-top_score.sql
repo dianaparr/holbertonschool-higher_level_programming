9041722127e8328a5c20cdffec4e954aea3092d9
dianaparr <dianaparra.9611@gmail.com>
2021-07-12 13:34:34 -0500

    Lists all records of the table with data ordered

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- Lists all records of the table - MySQL server
+-- The statements should display both the score (ordered, top first) and the name
+SELECT score, name
+FROM second_table
+ORDER BY score DESC;
