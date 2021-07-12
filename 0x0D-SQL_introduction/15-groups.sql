d48a1927b619af0ff3f6bb28373957029cd90c95
dianaparr <dianaparra.9611@gmail.com>
2021-07-12 15:42:39 -0500

    Update comments

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,5 +1,4 @@
--- Average - MySQL server
--- Statement that computes the score average of all records in the table
+-- Lists the number of records - MySQL server
 -- Clause GROUP BY: to join rows that have the same scores
 SELECT score, COUNT(score) as number
 FROM second_table
