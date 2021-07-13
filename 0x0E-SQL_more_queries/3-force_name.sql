dca9c240e57368467230086e8bf18f601b84075c
dianaparr <dianaparra.9611@gmail.com>
2021-07-13 13:18:43 -0500

    Creates the table and use of the constraint NOT NULL

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,5 @@
+-- Creates a table - On MySQL server
+-- Statement for creates a table called force_name in the current database
+-- Use of NOT NULL constraint
+CREATE TABLE IF NOT EXISTS force_name (id INT,
+name VARCHAR(256) NOT NULL);
