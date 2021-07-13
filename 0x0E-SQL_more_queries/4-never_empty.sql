20c2d4ec9aed091a75f4001f1714174f347ef3b4
dianaparr <dianaparra.9611@gmail.com>
2021-07-13 13:47:26 -0500

    Creates the table with using DEFAULT constraint

---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,5 @@
+-- Creates a table - On MySQL server
+-- Statement for creates a table called id_not_null in the current database
+-- Use of DEFAULT constraint
+CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT 1,
+name VARCHAR(256));
