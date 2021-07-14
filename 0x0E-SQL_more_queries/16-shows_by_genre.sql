7906c85140459e5a52c7feaef09e4da7ad8438dd
dianaparr <dianaparra.9611@gmail.com>
2021-07-13 21:39:40 -0500

    JOIN correction for NULL genres names

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -4,5 +4,5 @@
 SELECT tv_shows.title, tv_genres.name
 FROM tv_show_genres
 RIGHT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-RIGHT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 ORDER BY tv_shows.title, tv_genres.name ASC;
