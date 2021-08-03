09a18df948bd20d2f80672e7c9feb83244680a88
dianaparr <dianaparra.9611@gmail.com>
2021-08-02 19:20:10 -0500

    Update a data of an object

---
+++ 0x12-javascript-warm_up/12-object.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+// other form of set(update) the value myObject['value'] = 89;
+myObject.value = 89;
+console.log(myObject);
