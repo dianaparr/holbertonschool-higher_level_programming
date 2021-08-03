3ac80d47077bc9ea24a5545b9e606525b6270fd8
dianaparr <dianaparra.9611@gmail.com>
2021-08-03 12:10:43 -0500

    Adding statement for create a new object

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) { // if w or h is a positive integer create a object
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+module.exports = Rectangle;
