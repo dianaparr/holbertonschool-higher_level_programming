be8c8fb7a74263032cb3504ac94e61f642d54b41
dianaparr <dianaparra.9611@gmail.com>
2021-08-07 00:24:41 -0500

    Return to the first previous

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,11 +6,25 @@ import MySQLdb
 from sys import argv
 
 
-if __name__ == '__main__':
+def main():
+    """ Lists all states from the database hbtn_0e_0usa
+
+        Connecting, creating and read a MySQL DB - methods:
+            connect(parameters...) -> Use constructor for creating
+                                      a connection to the DB that take
+                                      3 arguments of the command line:
+                                      [1]: username, [2]: password and
+                                      [3]: DB name.
+            cursor() -> The MySQLCursor class instantiates objects that can
+                        execute operations such as SQL statements
+            execute() -> Executes the given database operation
+                         (query or command)
+            fetchall() -> It fetches all the rows in a result set
+    """
     # Data base connection
     connectDB = MySQLdb.connect(host="localhost", user=argv[1],
                                 passwd=argv[2], db=argv[3],
-                                port=3306)
+                                port=3306, charset="utf8")
     # Query to DB using execute method of the created cursor
     cursor = connectDB.cursor()
     queryDB = "SELECT * FROM states ORDER BY id ASC"
@@ -21,3 +35,6 @@ if __name__ == '__main__':
         print(row)
     # Closing the connection
     connectDB.close()
+
+if __name__ == '__main__':
+    main()
