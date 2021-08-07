b5d0ba4a5bc1805cd8637284958aaa35bd8fe0f1
dianaparr <dianaparra.9611@gmail.com>
2021-08-07 00:13:07 -0500

    Adding parenthesis in import engine

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -4,9 +4,8 @@ Module of python called model_state_fetch_all
 """
 from sys import argv
 from model_state import Base, State
-from sqlalchemy import create_engine
+from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker, Session
-
 
 def main():
     """ Lists all State objects from the database hbtn_0e_6_usa
