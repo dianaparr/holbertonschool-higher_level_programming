f38e3ea45de20aea66c718ac4b05cfd48b790f6a
dianaparr <dianaparra.9611@gmail.com>
2021-08-19 13:38:13 -0500

    Check script in sandbox

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Script that show the size of the body of the response
-# Parameter $1 0.0.0.0:5000
-curl -sI "$1" | grep Content-Length | cut -d' ' -f 2
+# Script that show the size of the body of the response: Parameter $1 0.0.0.0:5000
+curl -s -I "$1" | grep Content-Length | cut -d' ' -f2
