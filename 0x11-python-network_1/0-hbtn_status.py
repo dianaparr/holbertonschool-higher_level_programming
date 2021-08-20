f40038907d7ed44c51b4bcd26e2653dbc9b0f752
dianaparr <dianaparra.9611@gmail.com>
2021-08-20 09:17:12 -0500

    Uploading task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Module that fetches a URL """
+import urllib.request
+
+if __name__ == "__main__":
+    req = urllib.request.Request('https://intranet.hbtn.io/status')
+    with urllib.request.urlopen(req) as resp:
+        page = resp.read()
+        print("Body response:\n\
+        - type: {}\n\
+        - content: {}\n\
+        - utf8 content:{}".format(
+            type(page), page, page.decode('utf-8')))
