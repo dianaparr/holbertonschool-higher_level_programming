93c1c6a8742058f5b5f52ba1efc56c698c156982
dianaparr <dianaparra.9611@gmail.com>
2021-08-20 12:55:43 -0500

    Uploading task 4 relationated with requests module Py

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Module that fetches a URL - Import the Requests module """
+import requests
+
+if __name__ == "__main__":
+    r = requests.get('https://intranet.hbtn.io/status')
+    # The text encoding guessed by Requests is used when
+    # you access r.text
+    print("Body response:\n\
+        - type: {}\n\
+        - content: {}".format(type(r.text), r.text))
