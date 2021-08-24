585110aedd2c483c7c7fa573e1fa749a38054469
dianaparr <dianaparra.9611@gmail.com>
2021-08-24 18:00:36 -0500

    Changing range in for loop

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
                          format(sys.argv[2], sys.argv[1]))
         response_json = r.json()
         # print(response_json[0]['commit']['author']['name'])
-        for commit in range(0, 11):
+        for commit in range(10):
             print("{}: {}".format(response_json[commit].get('sha'),
                                   response_json[commit].get('commit').
                                   get('author').get('name')))
