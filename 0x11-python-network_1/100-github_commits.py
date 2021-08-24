f2d19c297eee502a043f16faf3e3ac2f89289590
dianaparr <dianaparra.9611@gmail.com>
2021-08-24 14:33:53 -0500

    Uploading task advanced

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Script that takes 2 arguments with GitHub API
+"""
+import requests
+import requests.auth
+import sys
+
+if __name__ == "__main__":
+    try:
+        r = requests.get('https://api.github.com/repos/{}/{}/commits'.
+                         format(sys.argv[2], sys.argv[1]))
+        response_json = r.json()
+        for commit in range(0, 11):
+            print("{}: {}".format(response_json[commit].get('sha'),
+                                  response_json[commit].
+                                  get('author').get('name')))
+    except Exception:
+        print(end="")
