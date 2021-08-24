5cfd1165c515292f9159889c678eba35cc3b8175
dianaparr <dianaparra.9611@gmail.com>
2021-08-24 17:59:17 -0500

    Fixing path to get 'name'

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -9,9 +9,10 @@ if __name__ == "__main__":
         r = requests.get('https://api.github.com/repos/{}/{}/commits'.
                          format(sys.argv[2], sys.argv[1]))
         response_json = r.json()
+        # print(response_json[0]['commit']['author']['name'])
         for commit in range(0, 11):
             print("{}: {}".format(response_json[commit].get('sha'),
-                                  response_json[commit].
+                                  response_json[commit].get('commit').
                                   get('author').get('name')))
     except:
         print("", end="")
