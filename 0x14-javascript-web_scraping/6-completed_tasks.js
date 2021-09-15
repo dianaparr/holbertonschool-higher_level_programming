4d0e26e2100f1aff03e37f16fb6f62e659893a38
dianaparr <dianaparra.9611@gmail.com>
2021-09-14 22:15:18 -0500

    New output, create dicc and put data

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const request = require("request");
+const request = require('request');
 const url = process.argv[2];
 
 request(url, function (err, response, body) {
@@ -9,10 +9,18 @@ request(url, function (err, response, body) {
     const dataJSON = JSON.parse(body);
     const newDict = {};
     for (const task of dataJSON) {
-      if (task.completed === true) {
-        newDict = newDict[task.userId];
-        console.log(newDict);
+      const taskCompleted = task.completed;
+      const emploId = task.userId;
+      // if taskCompleted is true
+      if (taskCompleted === true) {
+        // if emploId exists, sum one more (loop), the otherwise put one (first thing to do)
+        if (newDict[emploId]) {
+          newDict[emploId] += 1;
+        } else {
+          newDict[emploId] = 1;
+        }
       }
     }
+    console.log(newDict);
   }
 });
